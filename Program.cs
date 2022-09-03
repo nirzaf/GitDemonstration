e8a67b065fc4a9112bcfa7258a9d2a5775b836ac
MFM_Fazrin <mmfazrin@phcc.gov.qa>
2022-09-03 14:08:33 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace GitDemonstration
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
