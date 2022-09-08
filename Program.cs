e03343ddd80043b056404d8c67dac10439b3edea
niflan-mhd <niflannif2@gmail.com>
2022-09-08 22:05:09 +0530

    Add a new class

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Program myProgram = new Program();
+            myProgram.revers_no();
         }
 
         public string ConvertToUpperCase(string name)
@@ -25,6 +27,12 @@
         public void AddSecondNewMethod()
         {
             Console.WriteLine("AddSecondNewMethod");
+        }
+
+        public void revers_no()
+        {
+            ReverseNo a1 = new ReverseNo();
+            a1.print_revers_no();
         }
 
     }
