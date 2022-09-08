9555c5ea6f78cf7d87b4c833d24134006852a83b
Afrath49 <afrath4964@gmail.com>
2022-09-08 17:53:48 +0530

    Add a print function to the customer class

--- Customer.cs
+++ Customer.cs
@@ -8,6 +8,18 @@ namespace GitDemonstration
 {
     internal class Customer
     {
+        public string Name { get; set; }
+        public string Address { get; set; }
 
+        public Customer(string Name , string Address)
+        {
+            this.Name = Name;
+            this.Address = Address;
+        }
+
+        public void print()
+        {
+             Console.WriteLine("Customer Name is {0} and Address is {1}", Name, Address);
+        }
     }
 }
--- Program.cs
+++ Program.cs
@@ -5,6 +5,7 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+
         }
 
         public string ConvertToUpperCase(string name)
