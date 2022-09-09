1aae75504b457a7b8b3678032261a53a67c0bc41
Dilka Prabhashi <Dilka Prabhashi@DESKTOP-N9TS8UN>
2022-09-09 22:06:53 +0530

    Add a new Class

--- Program.cs
+++ Program.cs
@@ -5,6 +5,8 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            FindBiggestNum findBiggestNum = new FindBiggestNum();
+            findBiggestNum.BiggestNum();
         }
 
         public string ConvertToUpperCase(string name)
